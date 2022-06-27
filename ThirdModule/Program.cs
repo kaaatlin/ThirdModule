52bc6f868610a80e5aaa30a99fde45dca8af7043
Катерина С <sidorcheva2001@mail.ru>
2022-06-27 17:30:40 +0300

    Добавьте файлы проекта.

---
+++ ThirdModule/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ThirdModule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HavePet = true;
+            double FootSize = 37.5;
+
+            Console.WriteLine(MyName);
+            Console.WriteLine("Мне {0} лет", MyAge);
+            Console.WriteLine("Есть ли у меня животное? " + HavePet);
+            Console.WriteLine("Мой размер ноги " + FootSize);
+
+
+            Console.ReadKey();
+        }
+    }
+}
