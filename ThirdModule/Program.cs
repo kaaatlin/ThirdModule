ab0df9ccc6da4c7aacf66aa54a1c2f24ef7b846b
Катерина С <sidorcheva2001@mail.ru>
2022-06-27 18:19:57 +0300

    Добавлено перечисление с цветами и вариант вывода с помощью символа $

--- ThirdModule/Program.cs
+++ ThirdModule/Program.cs
@@ -4,6 +4,13 @@ namespace ThirdModule
 {
     internal class Program
     {
+        enum Semaphore
+        {
+
+            Red = 100,
+            Yellow = 200,
+            Green = 300,
+        }
         static void Main(string[] args)
         {
             string MyName = "Jane";
@@ -11,13 +18,19 @@ namespace ThirdModule
             bool HavePet = true;
             double FootSize = 37.5;
 
-            Console.WriteLine(MyName);
+            Console.WriteLine($"Мое имя {MyName}");
             Console.WriteLine("Мне {0} лет", MyAge);
             Console.WriteLine("Есть ли у меня животное? " + HavePet);
             Console.WriteLine("Мой размер ноги " + FootSize);
 
+           Semaphore ColorSemaphore = Semaphore.Yellow;
+            Console.WriteLine(ColorSemaphore);
 
             Console.ReadKey();
+
+     
         }
     }
+        
 }
+
